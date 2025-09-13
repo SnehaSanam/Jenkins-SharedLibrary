9e9385dbaa7b8385c54ef413848c2cfac60bdabf
SnehaSanam <snehassanam@gmail.com>
2025-09-13 21:21:49 +1000

    Update docker-images-build.groovy

--- vars/docker-images-build.groovy
+++ vars/docker-images-build.groovy
@@ -1,9 +1,10 @@
- def call(String dockerRegistry, String imageTag) {
+ def call(String FRONTEND_IMAGE, String BACKEND_IMAGE, String DOCKER_TAG) {
     stage('Docker Build') {
         echo "Building Docker images..."
-        sh """
-            docker build -t ${dockerRegistry}/chattingo-frontend:${imageTag} ./frontend
-            docker build -t ${dockerRegistry}/chattingo-backend:${imageTag} ./backend
-        """
+         sh """
+                    docker build -t ${FRONTEND_IMAGE}:${DOCKER_TAG} -f frontend/Dockerfile ./frontend
+                    docker build -t ${BACKEND_IMAGE}:${DOCKER_TAG} -f backend/Dockerfile ./backend
+                    docker images
+                """
     }
 }
