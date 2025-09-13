f275c3ed69ae68a77d88642bab606fef08568f3f
SnehaSanam <snehassanam@gmail.com>
2025-09-13 21:38:38 +1000

    Create docker_push_frontend_backend.groovy

---
+++ vars/docker_push_frontend_backend.groovy
@@ -0,0 +1,16 @@
+stage('Push to Docker Hub') {
+                withCredentials([usernamePassword(
+                    credentialsId: "dockerhub_creds",
+                    usernameVariable: "DOCKER_HUB_USER",
+                    passwordVariable: "DOCKER_HUB_PASS"
+                 )]) {
+                sh """
+                    echo $DOCKER_HUB_PASS | docker login -u $DOCKER_HUB_USER --password-stdin
+                    docker tag ${FRONTEND_IMAGE}:${DOCKER_TAG} $DOCKER_HUB_USER/${FRONTEND_IMAGE}:${DOCKER_TAG}
+                    docker tag ${BACKEND_IMAGE}:${DOCKER_TAG} $DOCKER_HUB_USER/${BACKEND_IMAGE}:${DOCKER_TAG}
+                    docker push $DOCKER_HUB_USER/${FRONTEND_IMAGE}:${DOCKER_TAG}
+                    docker push $DOCKER_HUB_USER/${BACKEND_IMAGE}:${DOCKER_TAG}
+                """
+            }
+        }    
+  
