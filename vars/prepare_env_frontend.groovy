a6f299bff5a79bf58fd4f007f478b372ff56e6d7
SnehaSanam <snehassanam@gmail.com>
2025-09-19 13:50:31 +1000

    Update prepare_env_frontend.groovy

--- vars/prepare_env_frontend.groovy
+++ vars/prepare_env_frontend.groovy
@@ -1,5 +1,9 @@
-def frontendEnv = """
+// vars/prepare_env_frontend.groovy
+def call() {
+    def frontendEnv = """
 REACT_APP_API_URL=https://chattingo-app.duckdns.org
 """
-      writeFile file: 'frontend/.env.production', text: frontendEnv
-      echo 'Frontend .env.production created'
+    writeFile file: 'frontend/.env.production', text: frontendEnv
+    echo 'Frontend .env.production created'
+}
+
